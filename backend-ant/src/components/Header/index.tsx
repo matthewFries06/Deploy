deda71d278688cb4f2f55e71cf296261d225533f
matthewFries06 <m.wierzchu@gmail.com>
2026-09-05 17:16:07 +0200

    Update index.tsx

--- backend-ant/src/components/Header/index.tsx
+++ backend-ant/src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import { HeaderClient } from './index.client'
 
 export async function Header() {
   const header = await getCachedGlobal('header', 1)()
+  const menu = header?.navItems || []
 
   return <HeaderClient header={header} />
 }
