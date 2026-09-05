da5b57c58674f0fa00ba4859d5ff1e1a90df56c7
matthewFries06 <m.wierzchu@gmail.com>
2026-09-05 17:30:04 +0200

    Update page.tsx

--- backend-ant/src/app/(app)/login/page.tsx
+++ backend-ant/src/app/(app)/login/page.tsx
@@ -9,6 +9,7 @@ import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import { LoginForm } from '@/components/forms/LoginForm'
 import { redirect } from 'next/navigation'
+export const dynamic = 'force-dynamic'
 
 export default async function Login() {
   const headers = await getHeaders()
