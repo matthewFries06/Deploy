8059fbd74ed1ac56e843b663082568e15598ccab
matthewFries06 <m.wierzchu@gmail.com>
2026-09-05 17:18:02 +0200

    Update page.tsx

--- backend-ant/src/app/(app)/checkout/page.tsx
+++ backend-ant/src/app/(app)/checkout/page.tsx
@@ -4,6 +4,7 @@ import { mergeOpenGraph } from '@/utilities/mergeOpenGraph'
 import React, { Fragment } from 'react'
 
 import { CheckoutPage } from '@/components/checkout/CheckoutPage'
+export const dynamic = 'force-dynamic'
 
 export default function Checkout() {
   return (
