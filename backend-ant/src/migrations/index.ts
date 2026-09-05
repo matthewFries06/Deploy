9208f934ec6e234dd15d92b492cb7ad0ed59c60f
unknown <m.wierzchu@gmail.com>
2026-09-05 19:54:20 +0200

    migrations add

---
+++ backend-ant/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260905_175342 from './20260905_175342';
+
+export const migrations = [
+  {
+    up: migration_20260905_175342.up,
+    down: migration_20260905_175342.down,
+    name: '20260905_175342'
+  },
+];
