29fbdd9ee8816620f5b1be5ca97495cb9dd7691b
matthewFries06 <m.wierzchu@gmail.com>
2026-09-05 17:21:42 +0200

    Update index.tsx

--- backend-ant/src/components/Footer/index.tsx
+++ backend-ant/src/components/Footer/index.tsx
@@ -12,7 +12,6 @@ const { COMPANY_NAME, SITE_NAME } = process.env
 export async function Footer() {
   const footer: Footer = await getCachedGlobal('footer', 1)()
   const menu = footer?.navItems || []
-  const menu = footer.navItems || []
   const currentYear = new Date().getFullYear()
   const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '')
   const skeleton = 'w-full h-6 animate-pulse rounded bg-neutral-200 dark:bg-neutral-700'
